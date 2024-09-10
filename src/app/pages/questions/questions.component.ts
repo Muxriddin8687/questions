736f0ebc0d3eae480fe61d28570f1cdc7854d369
muxriddin8687 <muxriddinsatipboyev@gmail.com>
2024-09-10 23:57:19 +0500

    fixed code

--- src/app/pages/questions/questions.component.ts
+++ src/app/pages/questions/questions.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from "@angular/core";
-import { BaseComponent } from "@core/components/base.component";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { ButtonModule } from "primeng/button";
 import { QuestionService } from "@services/question.service";
 import { JsonPipe } from "@angular/common";
@@ -8,24 +6,25 @@ import { AddFormComponent } from "./components/add-form/add-form.component";
 import { FilterComponent } from "./components/filter/filter.component";
 import { PaginatorModule } from "primeng/paginator";
 import { QuestionComponent } from "./components/question/question.component";
+import { DialogService } from "primeng/dynamicdialog";
 
-@UntilDestroy()
 @Component({
   selector: "app-questions",
   standalone: true,
   imports: [ButtonModule, JsonPipe, FilterComponent, PaginatorModule, QuestionComponent],
   templateUrl: "./questions.component.html",
+  providers: [DialogService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuestionsComponent extends BaseComponent implements OnInit {
+export class QuestionsComponent implements OnInit {
   protected _questionService = inject(QuestionService);
+  private _modalService = inject(DialogService);
   data = computed(() => this._questionService.data());
   filter = { sort: "id,asc", size: 15, page: 0 };
   start_page = 0;
 
   ngOnInit(): void {
     this.loadData();
-    this.start_page = 3;
   }
 
   loadData() {
@@ -43,26 +42,6 @@ export class QuestionsComponent extends BaseComponent implements OnInit {
     this.filter = { ...this.filter, ...event, page: 0 };
     this.start_page = 0;
     this.loadData();
-  }
-
-  override delete(id: number) {
-    this._questionService
-      .delete(id)
-      .pipe(untilDestroyed(this))
-      .subscribe({
-        next: () => this.successMessage("Savol o'chirildi."),
-        error: () => this.errorMessage(),
-      });
-  }
-
-  update(id: number) {
-    this._modalService.open(AddFormComponent, {
-      data: {
-        id: id,
-      },
-      header: "Savolni tahrirlash",
-      breakpoints: { "600px": "90vw", "960px": "75vw", "2500px": "500px" },
-    });
   }
 
   openAddModal() {
